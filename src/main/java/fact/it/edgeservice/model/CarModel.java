fb451bc94bd3ce806df924f7be9670442fd0634e
Pieter Pauwels <r0796064@student.thomasmore.be>
2022-01-02 09:43:49 +0100

    Full code coverage

--- src/main/java/fact/it/edgeservice/model/CarModel.java
+++ src/main/java/fact/it/edgeservice/model/CarModel.java
@@ -17,31 +17,15 @@ public class CarModel {
         return year;
     }
 
-    public void setYear(String year) {
-        this.year = year;
-    }
-
     public String getType() {
         return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public String getEngine() {
         return engine;
     }
 
-    public void setEngine(String engine) {
-        this.engine = engine;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
